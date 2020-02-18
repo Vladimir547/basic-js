551a55114d1115ab4b59ff7e85ad628ad145771e
Vladimir547 <germes547@gmail.ru>
2020-02-18 16:05:27 +0300

    feat: solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+	let res = '';
+	let deafaultRepeatTimes = !options.repeatTimes ? 1 : options.repeatTimes;
+	let defaultSeporator = !options.separator ? '+' : options.separator;
+	let defaultAddition = typeof options.addition === 'undefined' ? '' : options.addition;
+	let defaultAdditionRepeatTimes = !options.additionRepeatTimes ? 1 : options.additionRepeatTimes;
+	let defaultAdditionSeparator = !options.additionSeparator ? '' : options.additionSeparator;
+		for( let i = 0; i < deafaultRepeatTimes; i++){
+			res += str;
+			for (let j = 0; j < defaultAdditionRepeatTimes; j++){
+				res += defaultAddition;
+				if (j + 1 < defaultAdditionRepeatTimes){
+					res += defaultAdditionSeparator;
+				}
+			}
+			if (i + 1 < deafaultRepeatTimes){
+				res += defaultSeporator;
+			}
+		}
+		return res;
 };
   

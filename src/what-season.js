3f003b2fbefde9add77aa7ae55fd2fd4543328cd
Vladimir547 <germes547@gmail.ru>
2020-02-18 16:42:54 +0300

    feat: solution what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,22 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let winter = 'winter';
+  let spring = 'spring';
+  let summer = 'summer';
+  let autumn = 'autumn';
+  let result = '';
+  if(!date){
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.prototype.toString.call(date) !== "[object Date]"){
+    throw new Error;
+  }
+  if(date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11){
+    return winter;
+  } else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4){
+    return spring;
+  } else if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7){
+    return summer;
+  } else {
+    return autumn;
+  }
 };

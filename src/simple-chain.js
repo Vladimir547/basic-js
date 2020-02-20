5d655ef8505b9b87c044b819c91c7c10ff6f530b
Vladimir547 <germes547@gmail.ru>
2020-02-20 14:25:56 +0300

    feat: solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,33 @@
 const chainMaker = {
+  result: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.result.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value == null) {
+      this.result.push('null');
+    } else {
+      this.result.push(value);
+    }
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position >= this.getLength() || position < 1 || typeof position != 'number') {
+      this.result=[];
+      throw new Error;
+    } else {
+      this.result.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let str = '( '+this.result.join(' )~~( ')+' )';
+    this.result = [];
+    return str;
   }
 };
 
